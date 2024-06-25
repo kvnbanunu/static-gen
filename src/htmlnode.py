44cbf59deae17f64c3eca6d063e61e5d71d0ba6b
kvnbanunu <kvnbanunu@gmail.com>
2024-06-25 00:18:04 -0700

    Added ParentNode and conversion method in TextNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -6,7 +6,7 @@ class HTMLNode:
         self.props = props #dict of attributes
 
     def to_html(self):
-        raise NotImplementedError("Method not overridden")
+        raise NotImplementedError("to_html method not implemented")
 
     def props_to_html(self):
         if self.props is None:
@@ -25,10 +25,27 @@ class LeafNode(HTMLNode):
 
     def to_html(self):
         if self.value is None:
-            raise ValueError("LeafNode requires a value")
+            raise ValueError("Invalid HTML: no value")
         if self.tag is None:
             return self.value
         return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
 
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("Invalid HTML: no tag")
+        if self.children is None:
+            raise ValueError("Invalid HTML: no children")
+        children_html = ""
+        for child in self.children:
+            children_html += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,12 @@
+from htmlnode import LeafNode
+
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_link = "link"
+text_type_image = "image"
+
 class TextNode:
     def __init__(self, text, text_type, url=None):
         self.text = text
@@ -5,15 +14,28 @@ class TextNode:
         self.url = url
 
     def __eq__(self, node):
-        if (
+        return (
             self.text == node.text
             and self.text_type == node.text_type
             and self.url == node.url
-        ):
-            return True
-        return False
+        )
 
     def __repr__(self):
         if self.url is None:
             return f"TextNode({self.text}, {self.text_type})"
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+def text_node_to_html_code(text_node):
+        if text_node.text_type == text_type_text:
+            return LeafNode(None, text_node.text)
+        if text_node.text_type == text_type_bold:
+            return LeafNode("b", text_node.text)
+        if text_node.text_type == text_type_italic:
+            return LeafNode("i", text_node.text)
+        if text_node.text_type == text_type_code:
+            return LeafNode("code", text_node.text)
+        if text_node.text_type == text_type_link:
+            return LeafNode("a", text_node.text)
+        if text_node.text_type == text_type_image:
+            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+        raise ValueError(f"Invalid text type: {text_node.text_type}")
