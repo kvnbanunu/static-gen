a84894c0c4dc29d06194b6fcfe4ba42da68853a7
kvnbanunu <kvnbanunu@gmail.com>
2024-06-24 23:10:30 -0700

    Added LeafNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -6,15 +6,29 @@ class HTMLNode:
         self.props = props #dict of attributes
 
     def to_html(self):
-        raise NotImplementedError("Not overwritten by child class")
+        raise NotImplementedError("Method not overridden")
 
     def props_to_html(self):
         if self.props is None:
             return ""
         props_string = ""
-        for node in self.props:
-            props_string += f'{node}="{self.props[node]}" '
-        return props_string.rstrip()
+        for key, value in self.props.items():
+            props_string += f' {key}="{value}"'
+        return props_string
 
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("LeafNode requires a value")
+        if self.tag is None:
+            return self.value
+        return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,12 +1,29 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
-    def test_eq(self):
-        node = HTMLNode("p", "hello, world", None, {"href": "https://www.google.com", "target": "_blank"})
-        print(node)
+    def test_to_html_props(self):
+        node = HTMLNode(
+            "div",
+            "Hello, world!",
+            None,
+            {"class": "greeting", "href": "https://boot.dev"},
+        )
+        self.assertEqual(
+            node.props_to_html(),
+            ' class="greeting" href="https://boot.dev"',
+        )
+
+    def test_to_html_no_children(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
+
 
 if __name__ == "__main__":
     unittest.main()
